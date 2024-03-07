7ace128f532df55208f63c29ff0a378b74bdd5f5
John <andreygdsn@gmail.com>
2024-03-07 10:13:44 -0300

    um monte de printf com a logo do adult swim [as] :D

---
+++ introducao-logica/treinos/display-[as]/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    printf("[as][as][as][as]                                                        [as][as][as][as]\n");
+    printf("[as][as][as][as]                                                        [as][as][as][as]\n");
+    printf("[as][as][as][as]                                                        [as][as][as][as]\n");
+    printf("[as][as]          [as][as][as][as][as][as]    [as][as][as][as][as][as]          [as][as]\n");
+    printf("[as][as]          [as][as][as][as][as][as]    [as][as][as][as][as][as]          [as][as]\n");
+    printf("[as][as]          [as][as][as][as][as][as]    [as][as][as][as][as][as]          [as][as]\n");
+    printf("[as][as]          [as][as]        [as][as]    [as][as]                          [as][as]\n");
+    printf("[as][as]                          [as][as]    [as][as]                          [as][as]\n");
+    printf("[as][as]          [as][as][as][as][as][as]    [as][as][as][as][as][as]          [as][as]\n");
+    printf("[as][as]          [as][as][as][as][as][as]    [as][as][as][as][as][as]          [as][as]\n");
+    printf("[as][as]          [as][as][as][as][as][as]    [as][as][as][as][as][as]          [as][as]\n");
+    printf("[as][as]          [as][as]        [as][as]                    [as][as]          [as][as]\n");
+    printf("[as][as]          [as][as]        [as][as]                    [as][as]          [as][as]\n");
+    printf("[as][as]          [as][as][as][as][as][as]    [as][as][as][as][as][as]          [as][as]\n");
+    printf("[as][as]          [as][as][as][as][as][as]    [as][as][as][as][as][as]          [as][as]\n");
+    printf("[as][as]          [as][as][as][as][as][as]    [as][as][as][as][as][as]          [as][as]\n");
+    printf("[as][as][as][as]                  [as][as]                              [as][as][as][as]\n");
+    printf("[as][as][as][as]                                                        [as][as][as][as]\n");
+    printf("[as][as][as][as]                                                        [as][as][as][as]\n");
+}
