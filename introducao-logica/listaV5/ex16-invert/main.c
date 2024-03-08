15dc8bbe33fd59278587f42d8ee33db010a24ca7
John <andreygdsn@gmail.com>
2024-03-07 21:29:17 -0300

    ex16 invert string

--- introducao-logica/listaV5/ex16-invert/main.c
+++ introducao-logica/listaV5/ex16-invert/main.c
@@ -3,12 +3,12 @@
 int main(){
     int num, numIn = 0, c, d, u;
     printf("Insira um numero de 3 algorismos: ");
-    scanf("%d", &num);
-    c = num/100;
-    d = (num%100)/10;
-    u = num%10;
+    scanf("%d", &num); //suponha num =431
+    c = num/100; //431/100 = 4
+    d = (num%100)/10; //431%100 = 31| 31/10 = 3
+    u = num%10; // 431%10 = 1
     numIn =  u*100+d*10+c;
-    
+
     printf("%d - %d",num, numIn);
     return 0;
 }
