9921392d80d0d2ca4268d81edf1f41a732810fad
John <andreygdsn@gmail.com>
2024-03-16 23:49:27 -0300

    av1 2014.1 Q3

--- introducao-logica/avaliacoes/av1/2014.1/mainQ3.c
+++ introducao-logica/avaliacoes/av1/2014.1/mainQ3.c
@@ -5,5 +5,22 @@ por média) precisa obter numa prova final para ser aprovado. Considere que são
 realizadas três provas durante o semestre e que para aprovação em final a média obtida
 de acordo com a equação abaixo precisa ser igual ou superior a 5.0. (Valor 2,0)
 MédiaFinal=((média aritmética das três avaliações parciais)x 2+(notada provafinal) x1)/3
-
 */
+#include <stdio.h>
+
+int main(){
+    float av1, av2, av3, pf, media;
+    printf("Insira a nota das tres avaliacoes semestrais do aluno:\\> ");
+    scanf("%f %f %f",&av1,&av2,&av3);
+    media = (av1+av2+av3)/3;
+    if(media>=7) printf("ALUNO APROVADO");
+    else{
+        pf = (5-(media*2)/3)*3;
+        if(pf > 10) printf("ALUNO REPROVADO");
+        else{
+            printf("NECESSARIO PROVA FINAL\nPONTOS PARA ALCANCAR A MEDIA(5.0): %f",pf);
+        }
+    }
+
+    return 0;
+}
