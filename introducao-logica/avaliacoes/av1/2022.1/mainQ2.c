328a14a8779b9dd0a4f4e1aec4b0e80aabf6288c
John <andreygdsn@gmail.com>
2024-03-18 00:41:30 -0300

    av1 2022.1 Q2

--- introducao-logica/avaliacoes/av1/2022.1/mainQ2.c
+++ introducao-logica/avaliacoes/av1/2022.1/mainQ2.c
@@ -6,3 +6,13 @@ os ponteiros de hora e minutos e a posição de repouso, equivalente a 00:00. As
 00:00 o marcador indica de ângulos indica 000 e 000. Às 06:00, o marcador de ângulos, indicará:
 180 e 000. Às 06:30, contudo o valor do marcador de ângulos será: 195 e 180. Você deve escrever
 um programa, que leia os valores do marcador de ângulos e calcule a hora indicada pelo relógio. */
+#include <stdio.h>
+
+int main(){
+    int aHora, aMinuto, hora, minuto;
+    scanf("%d %d", &aHora, &aMinuto);
+    hora = aHora/30;
+    minuto = aMinuto/6;
+    printf("%02d:%02d",hora,minuto);
+    return 0;
+}
