cfbf22ff732233b5937daf58f545287aed434387
John <andreygdsn@gmail.com>
2024-03-19 23:26:54 -0300

    AV1 2016.1 Q3

--- introducao-logica/avaliacoes/av1/2016.1/mainQ3.c
+++ introducao-logica/avaliacoes/av1/2016.1/mainQ3.c
@@ -20,3 +20,22 @@ Suponha	 que	 serão	 usadas	 na	 iluminação	 apenas	 lâmpadas	 de	 60W	 e	 e
 programa	em	C,	que	leia	a	classe	de	utilização	do	cômodo	e	suas	dimensões	(largura	e	
 comprimento),	 calcule	 e	 imprima	 o	 número	 de	 lâmpadas	 de	 60W	 necessárias	 para	 a	
 iluminação	do	cômodo.*/
+#include <stdio.h>
+
+int main(){
+    float larg, comp, area, lamp; int class;
+    printf("Insira a classe de utilizacao do comodo:\\> ");
+    scanf("%d",&class);
+    printf("Insira a largura e o comprimento do comodo:\\> ");
+    scanf("%f %f",&larg, &comp);
+    area= larg*comp;
+    switch(class){
+        case 1:lamp=(area*12)/60;break;
+        case 2:lamp=(area*15)/60;break;
+        case 3:lamp=(area*20)/60;break;
+        default: printf("valor invalido.");
+    }
+    if(lamp-(int)lamp>0){lamp = (int)lamp+1;}
+    printf("Serao necessarias %.0f lampadas de 60W", lamp);
+    return 0;
+}
