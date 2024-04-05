905ac1a95bec3e86f92d4147bc04d5a5bc470411
John <andreygdsn@gmail.com>
2024-04-05 12:16:18 -0300

    decBin

---
+++ introducao-logica/aulas/decbin/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int dec, num,bit;
+    long long int bin; 
+    long long int base;
+    printf("Bota a porra de um numero aqui krl:\\> ");
+    scanf("%d",&dec);
+    base = 1; bin = 0;
+    num = dec;
+    while(num>0){
+        bit = num%2;
+        num /=2;
+        bin+= bit*base;
+        base*=10;
+    }
+    printf("(%d)10 = (%lld)2",dec, bin);
+    return 0;
+}
