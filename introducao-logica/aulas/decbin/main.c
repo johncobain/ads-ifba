ae2cf6eeb3f3204cb4f614b6617813b923dfd49c
John <andreygdsn@gmail.com>
2024-04-11 21:17:55 -0300

    ex 50 variavel preprocessada

--- introducao-logica/aulas/decbin/main.c
+++ introducao-logica/aulas/decbin/main.c
@@ -7,13 +7,21 @@ int main(){
     printf("Bota a porra de um numero aqui krl:\\> ");
     scanf("%d",&dec);
     base = 1; bin = 0;
-    num = dec;
-    while(num>0){
+    // num = dec;
+    for(num = dec; num>0; num/=2){
         bit = num%2;
         num /=2;
         bin+= bit*base;
         base*=10;
     }
+
+    // while(num>0){
+    //     bit = num%2;
+    //     num /=2;
+    //     bin+= bit*base;
+    //     base*=10;
+    // }
+    
     // printf("(%d)10 = (%lld)2",dec, bin); //saporra dando erro ai mas funciona
     return 0;
 }
--- introducao-logica/listaV5/ex50-dice/main.c
+++ introducao-logica/listaV5/ex50-dice/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#define QNT_LADOS 20
 
 int main(){
-    int num, i, j;
+    int num, i, j, k;
     printf("Insira um numero:\\> ");
     scanf("%d",&num);
-    for(i=1;i<=6;i++){
-        for(j=1;j<=6;j++){
-            if(i+j==num){
-                printf("%d %d\n",i,j);
+    for(i=1;i<=QNT_LADOS;i++){
+        for(j=1;j<=QNT_LADOS;j++){
+            for(k=1;k<=QNT_LADOS;k++){
+                if(i+j+k==num){
+                    printf("%d %d %d\n",i,j,k);
+                }
             }
         }
     }
