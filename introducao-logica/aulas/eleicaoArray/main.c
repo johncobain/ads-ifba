257e5562d1c837e8448fcf0000075d86f8b3b80f
John <andreygdsn@gmail.com>
2024-04-19 13:45:14 -0300

    urna eletronica

--- introducao-logica/aulas/eleicaoArray/main.c
+++ introducao-logica/aulas/eleicaoArray/main.c
@@ -12,11 +12,13 @@ int main(){
         printf("----------------------------------------------------------\n");
         printf("|Insira o seu voto:\\> ");
         scanf("%d", &voto);
-        if(voto>=0&&voto<QNT_PROP)votos[voto]++;
+        if(voto>=0&&voto<QNT_PROP){
+            votos[voto]+=1;
+        }
     }while(voto>=0);
     printf("----------------------------------------------------------\n");
     for(int i = 0; i<QNT_PROP; i++){
-        printf("Voto %d: %d\n",i, votos[0]);
+        printf("Voto %d: %d\n",i, votos[i]);
     }
   
     return 0;
