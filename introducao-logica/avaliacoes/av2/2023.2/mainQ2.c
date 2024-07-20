3f284953bc1d2f257ec29b86b6b5474f17621030
John <andreygdsn@gmail.com>
2024-07-20 17:30:10 -0300

    av2 2023.2 q2

---
+++ introducao-logica/avaliacoes/av2/2023.2/mainQ2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define QTD_EXP 6
+
+int main(){
+    int n=2, mers;
+    int numExp = 0;
+    int i, divisores;
+    while (numExp<QTD_EXP){
+        divisores = 0;
+        for(i=1;i<=n;i++)if(n%i==0)divisores++;
+        if(divisores==2){
+            mers = 1;
+            for(i=1;i<=n;i++) mers*=2;
+            mers-=1;
+            divisores=0;
+            for(i=1;i<=mers;i++)if(mers%i==0)divisores++;
+            if(divisores==2){
+                printf("%d %d\n",n, mers);
+                numExp++;
+            }
+        }
+        n++;
+    }
+    return 0;
+}
