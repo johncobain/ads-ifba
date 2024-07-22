1a5c750ea18e8cad18e9b15d2202bff831d91ccd
John <andreygdsn@gmail.com>
2024-07-22 16:27:53 -0300

    av2 2016.2 q2

---
+++ introducao-logica/avaliacoes/av2/2016.2/mainQ2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define TAM 20
+
+int main(){
+    int serie[TAM];
+    int n, res, i, dupli;
+    for(n=0; n<TAM; n++){
+        if(n==0) serie[n] = 0;
+        else{
+            res = serie[n-1]-n;
+            dupli=0;
+            for(i=0;i<n;i++){
+                if(serie[i]==res)dupli++;
+            }
+            if(res>0&&dupli==0)serie[n]=res;
+            else serie[n] = serie[n-1]+n;
+        }
+    }
+    for(i=0; i<TAM; i++){
+        printf("%d ", serie[i]);
+    }
+    return 0;
+}
