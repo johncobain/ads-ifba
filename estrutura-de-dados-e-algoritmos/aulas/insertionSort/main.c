e796dab27af0c41a0bcf5c2e6b6fbba8a2cb1726
Andrey Gomes <andreygdsn@gmail.com>
2024-11-11 21:53:58 -0300

    insertion sort

--- estrutura-de-dados-e-algoritmos/aulas/insertionSort/main.c
+++ estrutura-de-dados-e-algoritmos/aulas/insertionSort/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <limits.h>
 
 int * generate_random_integers(int size){
     int * values = malloc(sizeof(int)*size);
@@ -23,6 +24,44 @@ void insertion_sort(int *A, int length){
     }
 }
 
+
+void merge(int *A, int p, int q, int r){
+    int n1 = q-p+1;
+    int n2 = r-q;
+    int *L = malloc((n1+1)*sizeof(int));
+    int *R = malloc((n2+1)*sizeof(int));
+    int i, j;
+    for(i = 0; i < n1; i++){
+        L[i] = A[p+i];
+    }
+    L[n1]= INT_MAX;
+    for(j=0; j < n2; j++){
+        R[j] = A[q+j+1];
+    }
+    R[n2]= INT_MAX;
+    i=j=0;
+    for(int k=p; k<=r; k++){
+        if(L[i]<=R[j]){
+            A[k] = L[i];
+            i++;
+        }else{
+            A[k] = R[j];
+            j++;
+        }
+    }
+    free(L);
+    free(R);
+}
+
+void merge_sort(int *A, int p, int r){
+    if(p<r){
+        int q = (p+r)/2;
+        merge_sort(A,p,q);
+        merge_sort(A,q+1,r);
+        merge(A,p,q,r);
+    }
+}
+
 void print_array(int *A, int length){
     for(int i = 0; i < length; i++){
         if(i<length-1)printf("%d, ",A[i]);
@@ -33,12 +72,17 @@ void print_array(int *A, int length){
 
 int main(){
     srand(time(NULL));
-    int length = 7;
+    int length = 500000;
     int * values  = generate_random_integers(length);
-    printf("Antes\n");
-    print_array(values,length);
+    // printf("Antes\n");
+    // print_array(values,length);
+    clock_t start = clock();
     insertion_sort(values,length);
-    printf("Depois\n");
-    print_array(values,length);
+    // merge_sort(values,0,length-1);
+    clock_t end = clock();
+    double seconds = (double)(end-start)/CLOCKS_PER_SEC;
+    printf("Se passaram %.2f\n", seconds);
+    // printf("Depois\n");
+    // print_array(values,length);
     return 0;
 }
