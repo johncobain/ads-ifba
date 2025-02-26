e0bdc56cff210229ff4cba90323bc236a9ae92d2
Andrey Gomes <andreygdsn@gmail.com>
2025-02-26 16:39:17 -0300

    arvore avl

--- estrutura-de-dados-e-algoritmos/treinos/binaryTree/main.c
+++ estrutura-de-dados-e-algoritmos/treinos/binaryTree/main.c
@@ -6,39 +6,92 @@ typedef struct Node{
 
   struct Node *left;
   struct Node *right;
+  int hight;
 } Node;
 
+Node *newNode(int data);
+Node *insert(Node *root, int data); 
+Node *remove(Node *root, int data);
+Node *balance(Node *root);
+Node *rightRotation(Node *root);
+Node *leftRotation(Node *root);
+Node *leftRightRotation(Node *root);
+Node *rightLeftRotation(Node *root);
+int hight(Node *root);
+int biggerHight(Node *root);
+int balanceFactor(Node *root);
+
+Node *newNode(int data){
+  Node *new = (Node*)malloc(sizeof(Node));
+  if(new){
+    new->data = data;
+    new->left = NULL;
+    new->right = NULL;
+    new->hight = 0;
+  }else{
+    printf("Memory allocation error\n");
+    exit(1);
+  }
+
+  return new;
+}
+
 Node *insert(Node *root, int data){
-  Node *temp = (Node*)malloc(sizeof(Node));
-  temp->data = data;
-  temp->left = NULL;
-  temp->right = NULL;
+  Node *temp = newNode(data);
 
   if(root == NULL){
-    root = temp;
+    return temp;
   }else{
-    Node *current = root;
-    Node *parent = NULL;
-    while(1){
-      parent = current;
-      if(data < parent->data){
-        current = current->left;
-
-        if(current == NULL){
-          parent->left = temp;
-          return root;
-        }
-        
-      }else{
-        current = current->right;
-
-        if(current == NULL){
-          parent->right = temp;
-          return root;
-        }
-      }
+    if(data < root->data){
+      root->left = insert(root->left, data);
+    }else{
+      root->right = insert(root->right, data);
     }
   }
+
+  root->hight = biggerHight(root)+1;
+
+  root = balance(root);
+
+  return root;
+}
+
+Node *remove(Node *root, int data){
+  if(root == NULL) return NULL;
+  if(root->data == data){
+    if(root->left == NULL && root->right == NULL){
+      free(root);
+      return NULL;
+    }else if(root->left != NULL && root->right != NULL){
+      Node *temp = root->right;
+      while(temp->left != NULL){
+        temp = temp->left;
+      }
+      root->data = temp->data;
+      temp->data = data;
+      root->right = remove(root->right, data);
+      return root;
+    }else{
+      Node *temp;
+      if(root->left != NULL){
+        temp = root->left;
+      }else{
+        temp = root->right;
+      }
+      free(root);
+      return temp;
+    }
+  }else{
+    if(data < root->data){
+      root->left = remove(root->left, data);
+    }else{
+      root->right = remove(root->right, data);
+    }
+  }
+
+  root->hight = biggerHight(root)+1;
+
+  root = balance(root);
 
   return root;
 }
@@ -78,6 +131,77 @@ void inorder_dec(Node *root){
   }
 }
 
+int hight(Node *root){
+  if(root == NULL) return -1;
+  return root->hight;
+}
+
+int biggerHight(Node *root){
+  return (root->left->hight > root->right->hight) ? root->left->hight : root->right->hight;
+}
+
+int balanceFactor(Node *root){
+  if(root == NULL) return 0;
+
+  return hight(root->left) - hight(root->right);
+}
+
+Node*  rightRotation(Node *root){
+  printf("Right Rotation\n");
+  Node *l, *r;
+  l = root->left;
+  r = root->right;
+
+  l->right = root;
+  root->left = r;
+
+  root->hight = biggerHight(root)+1;
+  l->hight = biggerHight(l)+1;
+
+  return l;
+}
+
+Node* leftRotation(Node *root){
+  printf("Left Rotation\n");
+  Node *l, *r;
+  r = root->right;
+  l = root->left;
+
+  r->left = root;
+  root->right = l;
+
+  root->hight = biggerHight(root)+1;
+  r->hight = biggerHight(r)+1;
+
+  return r;
+}
+
+Node* leftRightRotation(Node *root){
+  root->left = leftRotation(root->left);
+  return rightRotation(root);
+}
+
+Node* rightLeftRotation(Node *root){
+  root->right = rightRotation(root->right);
+  return leftRotation(root);  
+}
+
+Node* balance(Node *root){
+  int bf = balanceFactor(root);
+
+  if(bf< -1 && balanceFactor(root->right) <= 0){
+    root = leftRotation(root);
+  }else if(bf > 1 && balanceFactor(root->left) >= 0){
+    root = rightRotation(root);
+  }else if(bf > 1 && balanceFactor(root->left) < 0){
+    root = leftRightRotation(root);
+  }else if(bf < -1 && balanceFactor(root->right) > 0){
+    root = rightLeftRotation(root);
+  }
+    
+  return root;
+}
+
 int search(Node *root, int data){
   if(root == NULL) return NULL;
   
@@ -91,16 +215,18 @@ int search(Node *root, int data){
 int main(){
   int i;
   int array[14] = 
-  {34, 84, 15, 0, 2, 99, 79, 9, 88, 18, 31, 39, 100, 101};
+  {34, 84, 95, 100, 102, 99, 79, 9, 88, 18, 31, 39, 100, 101};
 
   Node *root = NULL;
   int qtdLeafs = 0;
 
   for(i=0; i< 14; i++){
     root = insert(root, array[i]);
-    printf("Inserted %d\n", array[i]);
+    printf("\nInserted %d\n", array[i]);
+    printf("Balance Factor: %d\n", balanceFactor(root));
+    printf("Root: %d\n", root->data);
+    printf("Hight: %d\n", hight(root));
   }
-
   printf("\nInorder traversal:\n");
   inorder_traversal(root);
   printf("\nPreorder:\n");
