c035725b4e1f1fab5b41a021382d19f77b9a13a9
Andrey <andreygdsn@gmail.com>
2025-05-22 14:54:17 -0300

    utils: colors and lines

---
+++ 03-semestre/POO/trabalhos/uniEvents/src/main/java/br/edu/ifba/inf0008/uniEvents/utils/Colors.java
@@ -0,0 +1,17 @@
+package br.edu.ifba.inf0008.uniEvents.utils;
+
+public class Colors {
+  public static final String RESET = "\033[0m";
+
+  public static final String RED = "\033[0;31m";   
+  public static final String GREEN = "\033[0;32m"; 
+  public static final String YELLOW = "\033[0;33m";
+  public static final String BLUE = "\033[0;34m";  
+  public static final String PURPLE = "\033[0;35m";
+
+  public static final String RED_BOLD = "\033[1;31m";    
+  public static final String GREEN_BOLD = "\033[1;32m";  
+  public static final String YELLOW_BOLD = "\033[1;33m"; 
+  public static final String BLUE_BOLD = "\033[1;34m";   
+  public static final String PURPLE_BOLD = "\033[1;35m";  
+}
---
+++ 03-semestre/POO/trabalhos/uniEvents/src/main/java/br/edu/ifba/inf0008/uniEvents/utils/Lines.java
@@ -0,0 +1,71 @@
+package br.edu.ifba.inf0008.uniEvents.utils;
+
+public class Lines {
+  public static String straightLine(){
+    return "----------------------------------------------------------------";
+  }
+  public static String doubleLine(){
+    return "================================================================";
+  }
+  public static String mixedLines(){
+    return "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-";
+  }
+
+  public static String clear(){
+    return "\033[H\033[2J";
+  }
+
+  public static String titleLine(String title){
+    int totalLenght = 62 - title.length();
+    int leftPadding = totalLenght/2;
+    int rightPadding = totalLenght - leftPadding;
+    String finalLine = "|";
+    for(int i =0; i<leftPadding; i++){
+      finalLine = finalLine + " ";
+    }
+    finalLine = finalLine + title;
+    for(int i =0; i<rightPadding; i++){
+      finalLine = finalLine + " ";
+    }
+    finalLine = finalLine + "|";
+    return finalLine;
+  }
+
+  public static String leftText(String text){
+    int totalLenght = 62 - text.length();
+    String finalLine = "|";
+    finalLine = finalLine + text;
+    for(int i =0; i<totalLenght; i++){
+      finalLine = finalLine + " ";
+    }
+    finalLine = finalLine + "|";
+    return finalLine;
+  }
+
+  public static String rightText(String text){
+    int totalLenght = 62 - text.length();
+    String finalLine = "|";
+    for(int i =0; i<totalLenght; i++){
+      finalLine = finalLine + " ";
+    }
+    finalLine = finalLine + text;
+    finalLine = finalLine + "|";
+    return finalLine;
+  }
+
+  public static String errorLine(String text){
+    int totalLenght = 62 - text.length();
+    int leftPadding = totalLenght/2;
+    int rightPadding = totalLenght - leftPadding;
+    String finalLine = "|";
+    for(int i =0; i<leftPadding; i++){
+      finalLine = finalLine + " ";
+    }
+    finalLine = finalLine + Colors.RED_BOLD + text + Colors.RESET;
+    for(int i =0; i<rightPadding; i++){
+      finalLine = finalLine + " ";
+    }
+    finalLine = finalLine + "|";
+    return finalLine;
+  }
+}
