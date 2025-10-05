038f58fe9eb1a8905e6f321a257ab25fdd098356
Andrey Gomes <andreygdsn@gmail.com>
2025-10-04 23:51:07 -0300

    return json

--- 04-semestre/PWEB/pwebspring/src/main/java/br/edu/ifba/pwebspring/controller/HelloController.java
+++ 04-semestre/PWEB/pwebspring/src/main/java/br/edu/ifba/pwebspring/controller/HelloController.java
@@ -4,12 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ifba.pwebspring.model.User;
+
 @RestController
 @RequestMapping("/")
 public class HelloController {
 	@GetMapping
 	public String HelloWorld() {
-        System.out.println("Hello World");
+    System.out.println("Hello World");
 		return "Hello World";
+	}
+
+	@GetMapping("/user")
+	public User user() {
+		return new User(1, "John Doe");
 	}
 }
---
+++ 04-semestre/PWEB/pwebspring/src/main/java/br/edu/ifba/pwebspring/model/User.java
@@ -0,0 +1,11 @@
+package br.edu.ifba.pwebspring.model;
+
+public class User {
+  public Integer id;
+  public String name;
+
+  public User(Integer id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+}
