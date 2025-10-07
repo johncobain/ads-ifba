286e36b161bb6c4d2f99fa00a379ff6a73f162a7
Andrey Gomes <andreygdsn@gmail.com>
2025-10-06 22:47:25 -0300

    factory method aula

--- 04-semestre/PADROES/aulas/factoryMethod/src/main/java/br/edu/ifba/inf011/App.java
+++ 04-semestre/PADROES/aulas/factoryMethod/src/main/java/br/edu/ifba/inf011/App.java
@@ -1,8 +1,6 @@
 package br.edu.ifba.inf011;
 
-import br.edu.ifba.inf011.drawing.DrawingApplication;
 import br.edu.ifba.inf011.json.JsonApplication;
-import br.edu.ifba.inf011.xml.XmlApplication;
 
 public class App {
     public static void run(Application app){
