3970ff98f441c0907a65ba85a5cfa22449a40519
Andrey Gomes <andreygdsn@gmail.com>
2025-10-09 23:20:31 -0300

    prototype

---
+++ 04-semestre/PADROES/exemplos/criacionais/prototype/src/main/java/br/edu/ifba/inf011/GameSettings.java
@@ -0,0 +1,46 @@
+package br.edu.ifba.inf011;
+
+public class GameSettings implements Cloneable {
+    private int brightness;
+    private int volume;
+    private String difficulty;
+
+    public GameSettings(int brightness, int volume, String difficulty) {
+        this.brightness = brightness;
+        this.volume = volume;
+        this.difficulty = difficulty;
+    }
+
+    @Override
+    protected GameSettings clone() throws CloneNotSupportedException {
+        try {
+            return (GameSettings) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public void setBrightness(int brightness) {
+        this.brightness = brightness;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+}
---
+++ 04-semestre/PADROES/exemplos/criacionais/prototype/src/main/java/br/edu/ifba/inf011/Main.java
@@ -0,0 +1,25 @@
+package br.edu.ifba.inf011;
+
+public class Main {
+    public static void main(String[] args) {
+        // Criando o protótipo de configurações do jogo
+        GameSettings prototypeSettings = new GameSettings(50, 70, "Normal");
+        System.out.println(prototypeSettings.hashCode());
+
+        // Clonando e modificando para um novo jogador
+        GameSettings playerSettings = null;
+        try {
+            playerSettings = prototypeSettings.clone();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(playerSettings.hashCode());
+        playerSettings.setDifficulty("Hard");
+        playerSettings.setBrightness(2);
+
+        System.out.println("Player Settings: Difficulty is " + playerSettings.getDifficulty());
+        System.out.println("Player Settings: Volume is " + playerSettings.getVolume());
+        System.out.println("Player Settings: brightness is " + playerSettings.getBrightness());
+        System.out.println("Prototype Settings: brightness is " + prototypeSettings.getBrightness());
+    }
+}
