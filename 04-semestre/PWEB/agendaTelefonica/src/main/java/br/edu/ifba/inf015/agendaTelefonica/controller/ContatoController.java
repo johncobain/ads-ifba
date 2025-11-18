617a7acd592f2ba5d6549a6b40202d962425384d
Andrey Gomes <andreygdsn@gmail.com>
2025-11-18 18:31:00 -0300

    remove telefoneController

--- 04-semestre/PWEB/agendaTelefonica/src/main/java/br/edu/ifba/inf015/agendaTelefonica/controller/ContatoController.java
+++ 04-semestre/PWEB/agendaTelefonica/src/main/java/br/edu/ifba/inf015/agendaTelefonica/controller/ContatoController.java
@@ -3,7 +3,11 @@ package br.edu.ifba.inf015.agendaTelefonica.controller;
 import br.edu.ifba.inf015.agendaTelefonica.dto.contato.ContatoDto;
 import br.edu.ifba.inf015.agendaTelefonica.dto.contato.ContatoFormDto;
 import br.edu.ifba.inf015.agendaTelefonica.dto.contato.ContatoUpdateDto;
+import br.edu.ifba.inf015.agendaTelefonica.dto.telefone.TelefoneDto;
+import br.edu.ifba.inf015.agendaTelefonica.dto.telefone.TelefoneFormDto;
+import br.edu.ifba.inf015.agendaTelefonica.dto.telefone.TelefoneUpdateDto;
 import br.edu.ifba.inf015.agendaTelefonica.model.entities.Usuario;
+import br.edu.ifba.inf015.agendaTelefonica.service.TelefoneService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,9 +23,11 @@ import java.util.List;
 @RequestMapping("/contatos")
 public class ContatoController {
   private final ContatoService contatoService;
+  private final TelefoneService telefoneService;
 
-  public ContatoController(ContatoService contatoService) {
+  public ContatoController(ContatoService contatoService, TelefoneService telefoneService) {
     this.contatoService = contatoService;
+    this.telefoneService = telefoneService;
   }
 
   @PostMapping
@@ -59,4 +65,31 @@ public class ContatoController {
     return ResponseEntity.noContent().build();
   }
 
+  @PostMapping("/{contatoId}/telefones")
+  public ResponseEntity<List<TelefoneDto>> addTelefones(
+      @PathVariable Long contatoId,
+      @Valid @RequestBody List<TelefoneFormDto> telefonesForm,
+      @AuthenticationPrincipal Usuario usuario){
+    return ResponseEntity.status(201)
+        .body(telefoneService.addTelefones(contatoId, telefonesForm, usuario));
+  }
+
+  @PutMapping("/{contatoId}/telefones/{telefoneId}")
+  public ResponseEntity<TelefoneDto> updateTelefone(
+      @PathVariable Long contatoId,
+      @PathVariable Long telefoneId,
+      @Valid @RequestBody TelefoneUpdateDto telefoneUpdate,
+      @AuthenticationPrincipal Usuario usuario){
+    return ResponseEntity.ok()
+        .body(telefoneService.updateTelefone(contatoId, telefoneId, telefoneUpdate, usuario));
+  }
+
+  @DeleteMapping("/{contatoId}/telefones/{telefoneId}")
+  public ResponseEntity<TelefoneDto> deleteTelefone(
+      @PathVariable Long contatoId,
+      @PathVariable Long telefoneId,
+      @AuthenticationPrincipal Usuario usuario){
+    telefoneService.deleteTelefone(contatoId, telefoneId, usuario);
+    return ResponseEntity.noContent().build();
+  }
 }
